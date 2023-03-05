d372cbaf85eb5815132c2f7f3460c4cf0537c8ee
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-03-06 01:13:58 +0600

    foods display in card, dynamic search bar, add animation

--- Meal Db project/mealdb.js
+++ Meal Db project/mealdb.js
@@ -2,23 +2,37 @@ const loadMeal = async (searchText) => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayMeal(data.meals[0]);
+    displayMeal(data.meals);
 }
 
 const displayMeal = (meals) => {
+    // console.log(meals);
     const mealContainer = document.getElementById('meal-container');
-    mealContainer.innerHTML = `
-        <div class="col">
-            <div class="card">
-                <img src="${meals.strMealThumb}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                            to additional content. This content is a little bit longer.</p>
-                    </div>
+    mealContainer.innerHTML = '';
+    meals.forEach((meal) => {
+        console.log(meal);
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+        <div class="card h-100">
+            <div class="overflow-hidden">
+                <img src="${meal.strMealThumb}" class="card-img-top img-animation" alt="...">
             </div>
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal} - ${meal.strArea}</h5>
+                    <p class="card-text">
+                        <i class="fa-solid fa-bangladeshi-taka-sign"></i><i class="fa-solid fa-bangladeshi-taka-sign"></i><i class="fa-solid fa-bangladeshi-taka-sign"></i>, 
+                        ${meal.strCategory}</p>
+                </div>
         </div>
     `;
+        mealContainer.appendChild(mealDiv);
+    });
+}
+
+const searchMeals = () => {
+    const searchText = document.getElementById('search-field').value;
+    loadMeal(searchText);
 }
 
 
@@ -32,8 +46,4 @@ const displayMeal = (meals) => {
 
 
 
-
-
-
-
-loadMeal('pasta');
+loadMeal('fish');
